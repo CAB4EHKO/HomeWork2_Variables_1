1de21fe9d675c83b2466b262314e190d20dc63fd
Savchenko <savchenko.ra@yandex.ru>
2023-02-06 12:38:55 +0300

    Creat variables.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        task1();
     }
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Собака: " + dog + "\nКот: " + cat + "\nБумага: " + paper);
+    }
+
 }
